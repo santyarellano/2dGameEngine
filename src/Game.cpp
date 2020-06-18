a4ab244fd1d49c3f9a15020e83ee812c8c0d3527
SSAM <ssam@SSAMs-MacBook-Pro.local>
2020-06-18 17:35:06 -0500

    First Commit (Entity Component)

---
+++ src/Game.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include "./Constants.h"
+#include "./Game.h"
+#include "../lib/glm/glm.hpp"
+
+EntityManager manager;
+SDL_Renderer* Game::renderer;
+
+Game::Game() {
+	this->isRunning = false;
+}
+
+Game::~Game() {
+}
+
+bool Game::IsRunning() const {
+	return this->isRunning;
+}
+
+void Game::Initialize(int width, int height) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		std::cerr << "Error initializing SDL." << std::endl;
+		return;
+	}
+	window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		width,
+		height,
+		SDL_WINDOW_BORDERLESS
+	);
+	if (!window) {
+		std::cerr << "Error creating SDL window." << std::endl;
+	}
+	renderer = SDL_CreateRenderer(window, -1,0);
+	if (!renderer) {
+		std::cerr << "Error creating SDL renderer." << std::endl;
+	}
+
+	isRunning = true;
+	return;
+}
+
+void Game::ProcessInput() {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch (event.type) {
+		case SDL_QUIT:
+			isRunning = false;
+			break;
+
+		case SDL_KEYDOWN:
+			if(event.key.keysym.sym == SDLK_ESCAPE) {
+				isRunning = false;
+			}
+		default:
+			break;
+	}
+}
+
+void Game::Update() {
+	// Sleep the execution until we reach the target frame in ms
+	int timeToWait = FRAME_TARGET_TIME - (SDL_GetTicks() - ticksLastFrame);
+
+	// Only call delay if we are too fast to process this frame
+	if (timeToWait > 0 && timeToWait <= (int)FRAME_TARGET_TIME) {
+		SDL_Delay(timeToWait);
+	}
+
+	// Delta time is the difference in ticks from last fram converted to seconds
+	float deltaTime = (SDL_GetTicks() - ticksLastFrame)/1000.0f;
+
+	// Clamp delta time to a maximum value
+	deltaTime = (deltaTime > 0.05f) ? 0.05f : deltaTime;
+	
+	// Sets the new ticks for the current fram to be used in the next pass.
+	ticksLastFrame = SDL_GetTicks();
+
+	// TODO:
+	// Here we call the manager.update to update all entities as a function of deltaTime
+}
+
+void Game::Render() {
+	// Set the background color
+	SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
+
+	// Clear the back buffer
+	SDL_RenderClear(renderer);
+
+	// Draw all objects of the scene
+	// TODO:
+	// Here we call the manager.render to render all entities
+
+	// Swap front and back buffers
+	SDL_RenderPresent(renderer);
+}
+
+void Game::Destroy() {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
---
+++ src/Game.h
@@ -0,0 +1,29 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include "./Entity.h"
+#include "./Component.h"
+#include "./EntityManager.h"
+
+class Game {
+	private:
+		bool isRunning;
+		SDL_Window *window;
+	public:
+		Game();
+		~Game();
+		int ticksLastFrame;
+		bool IsRunning() const;
+		static SDL_Renderer *renderer;
+		void LoadLevel(int levelNumber);
+		void Initialize(int width, int height);
+		void ProcessInput();
+		void Update();
+		void Render();
+		void Destroy();
+};
+
+#endif
