9d140b8a23280400fdd32b6ebc99307ddff77e1a
SSAM <ssam@SSAMs-MacBook-Pro.local>
2020-06-19 15:01:48 -0500

    Added animations

--- src/Game.cpp
+++ src/Game.cpp
@@ -51,13 +51,17 @@ void Game::Initialize(int width, int height) {
 
 void Game::LoadLevel(int levelNumber) {
 	// Start including new assets to the asset manager list
-	std::string textureFilePath = "./assets/images/tank-big-right.png";
-	assetManager->AddTexture("tank-image", textureFilePath.c_str());
-
+	assetManager->AddTexture("tank-image", std::string("./assets/images/tank-big-right.png").c_str());
+	assetManager->AddTexture("chopper-image", std::string("./assets/images/chopper-spritesheet.png").c_str());
+	
 	// Create entities
-	Entity& newEntity(manager.AddEntity("tank"));
-	newEntity.AddComponent<TransformComponent>(0, 0, 20, 20, 32, 32, 1);
-	newEntity.AddComponent<SpriteComponent>("tank-image");
+	Entity& tankEntity(manager.AddEntity("tank"));
+	tankEntity.AddComponent<TransformComponent>(0, 0, 20, 20, 32, 32, 1);
+	tankEntity.AddComponent<SpriteComponent>("tank-image");
+
+	Entity& chopperEntity(manager.AddEntity("chopper"));
+	chopperEntity.AddComponent<TransformComponent>(240,106,0,0,32,32,1);
+	chopperEntity.AddComponent<SpriteComponent>("chopper-image", 2, 90, true, false);
 
 	// print all entities data
 	manager.ListAllEntities();
