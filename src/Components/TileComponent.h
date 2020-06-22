88d51765d3e3481ffc741bea1d2167538d11182d
SSAM <ssam@SSAMs-MacBook-Pro.local>
2020-06-22 12:51:03 -0500

    Added map tiles

---
+++ src/Components/TileComponent.h
@@ -0,0 +1,48 @@
+#ifndef TILECOMPONENT_H
+#define TILECOMPONENT_H
+
+#include <SDL2/SDL.h>
+#include <string>
+#include "../EntityManager.h"
+#include "../../lib/glm/glm.hpp"
+#include "../AssetManager.h"
+
+class TileComponent: public Component {
+	public:
+		SDL_Texture *texture;
+		SDL_Rect sourceRectangle;
+		SDL_Rect destinationRectangle;
+		glm::vec2 position;
+
+		TileComponent(int soureRectX, int soureRectY, int x, int y, int tileSize, int tileScale, std::string assetTextureId) {
+			texture = Game::assetManager->GetTexture(assetTextureId);
+
+			sourceRectangle.x = soureRectX;
+			sourceRectangle.y = soureRectY;
+			sourceRectangle.w = tileSize;
+			sourceRectangle.h = tileSize;
+
+			destinationRectangle.x = x;
+			destinationRectangle.y = y;
+			destinationRectangle.w = tileSize * tileScale;
+			destinationRectangle.h = tileSize * tileScale;
+
+			position.x = x;
+			position.y = y;
+		}
+
+		~TileComponent() {
+			SDL_DestroyTexture(texture);
+		}
+
+		void Update(float deltaTime) override {
+			// TODO: here is where we will take care of the tile positions
+			// based on the camera control that will keep moving around.
+		}
+
+		void Render() override {
+			TextureManager::Draw(texture, sourceRectangle, destinationRectangle, SDL_FLIP_NONE);
+		}
+};
+
+#endif
--- src/Game.cpp
+++ src/Game.cpp
@@ -7,11 +7,13 @@
 #include "./Components/SpriteComponent.h"
 #include "./Components/KeyboardControlComponent.h"
 #include "../lib/glm/glm.hpp"
+#include "./Map.h"
 
 EntityManager manager;
 AssetManager* Game::assetManager = new AssetManager(&manager);
 SDL_Renderer* Game::renderer;
 SDL_Event Game::event;
+Map* map;
 
 Game::Game() {
 	this->isRunning = false;
@@ -56,7 +58,11 @@ void Game::LoadLevel(int levelNumber) {
 	assetManager->AddTexture("tank-image", std::string("./assets/images/tank-big-right.png").c_str());
 	assetManager->AddTexture("chopper-image", std::string("./assets/images/chopper-spritesheet.png").c_str());
 	assetManager->AddTexture("radar-image", std::string("./assets/images/radar.png").c_str());
-	
+	assetManager->AddTexture("jungle-tiletexture", std::string("./assets/tilemaps/jungle.png").c_str());
+
+	map = new Map("jungle-tiletexture", 2, 32);
+	map->LoadMap("./assets/tilemaps/jungle.map", 25, 20);
+
 	// Create entities
 	Entity& chopperEntity(manager.AddEntity("chopper"));
 	chopperEntity.AddComponent<TransformComponent>(240,106,0,0,32,32,1);
