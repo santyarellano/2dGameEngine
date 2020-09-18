574dd9cbeeef415b9cb382cb446b43e19e5e2892
SSAM <ssam@MacBook-Pro-de-SSAM.local>
2020-09-18 09:27:26 -0500

    removed cmake

--- src/AssetManager.h
+++ src/AssetManager.h
@@ -8,21 +8,23 @@
 #include "./FontManager.h"
 #include "./EntityManager.h"
 
-class AssetManager {
-    private:
-        EntityManager* manager;
-        std::map<std::string, SDL_Texture*> textures;
-        std::map<std::string, TTF_Font*> fonts;
-    public:
-        AssetManager(EntityManager* manager);
-        ~AssetManager();
-        void ClearData();
-        
-        void AddTexture(std::string textureId, const char* filePath);
-        void AddFont(std::string fontId, const char* filePath, int fontSize);        
+class AssetManager
+{
+private:
+    EntityManager *manager;
+    std::map<std::string, SDL_Texture *> textures;
+    std::map<std::string, TTF_Font *> fonts;
 
-        SDL_Texture* GetTexture(std::string textureId);
-        TTF_Font* GetFont(std::string fontId);
+public:
+    AssetManager(EntityManager *manager);
+    ~AssetManager();
+    void ClearData();
+
+    void AddTexture(std::string textureId, const char *filePath);
+    void AddFont(std::string fontId, const char *filePath, int fontSize);
+
+    SDL_Texture *GetTexture(std::string textureId);
+    TTF_Font *GetFont(std::string fontId);
 };
 
 #endif
--- src/Game.h
+++ src/Game.h
@@ -11,29 +11,31 @@
 
 class AssetManager;
 
-class Game {
-    private:
-        bool isRunning;
-        SDL_Window *window;
-    public:
-        Game();
-        ~Game();
-        int ticksLastFrame = 0;
-        bool IsRunning() const;
-        static SDL_Renderer *renderer;
-        static AssetManager* assetManager;
-        static SDL_Event event;
-        static SDL_Rect camera;
-        void LoadLevel(int levelNumber);
-        void Initialize(int width, int height);
-        void ProcessInput();
-        void ProcessNextLevel(int levelNumber);
-        void ProcessGameOver();
-        void Update();
-        void Render();
-        void Destroy();
-        void HandleCameraMovement();
-        void CheckCollisions();
+class Game
+{
+private:
+    bool isRunning;
+    SDL_Window *window;
+
+public:
+    Game();
+    ~Game();
+    int ticksLastFrame = 0;
+    bool IsRunning() const;
+    static SDL_Renderer *renderer;
+    static AssetManager *assetManager;
+    static SDL_Event event;
+    static SDL_Rect camera;
+    void LoadLevel(int levelNumber);
+    void Initialize(int width, int height);
+    void ProcessInput();
+    void ProcessNextLevel(int levelNumber);
+    void ProcessGameOver();
+    void Update();
+    void Render();
+    void Destroy();
+    void HandleCameraMovement();
+    void CheckCollisions();
 };
 
 #endif
