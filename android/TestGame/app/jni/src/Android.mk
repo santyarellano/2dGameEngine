b4c03553a22c28977e2e5365f49b51ab4ebe70bf
SSAM <ssam@MacBook-Pro-de-SSAM.local>
2020-09-18 19:47:29 -0500

    update

--- android/TestGame/app/jni/src/Android.mk
+++ android/TestGame/app/jni/src/Android.mk
@@ -9,7 +9,17 @@ SDL_PATH := ../SDL
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
 
 # Add your application source files here...
-LOCAL_SRC_FILES := YourSourceHere.c
+LOCAL_SRC_FILES := \
+        Animation.cpp
+        AssetManager.cpp
+        Collision.cpp
+        Entity.cpp
+        EntityManager.cpp
+        FontManager.cpp
+        Game.cpp
+        Main.cpp
+        Map.cpp
+        TextureManager.cpp
 
 LOCAL_SHARED_LIBRARIES := SDL2 \
 SDL2_image \
